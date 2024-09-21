13ee56ce889ab438447263cc63ca0562a14fb17a
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2024-09-21 21:21:27 +0600

    update cors

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,12 @@ import routes from './app/routes';
 const app: Application = express();
 
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'https://love-birds-bd.vercel.app'],
+    credentials: true
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
